fc458366a869d4c3aeeedab1a974f18cebb155c4
Mariana Martins <marih_ms@hotmail.com>
2024-06-08 11:36:45 -0300

    create class Funcionario

---
+++ src/entidades/Funcionario.java
@@ -0,0 +1,50 @@
+package entidades;
+
+public class Funcionario {
+    private String nome;
+    private int idade;
+    private Endereco endereco;
+    private double salario;
+
+    public Funcionario() {
+    }
+
+    public Funcionario(String nome, int idade, Endereco endereco, double salario) {
+        this.nome = nome;
+        this.idade = idade;
+        this.endereco = endereco;
+        this.salario = salario;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+}
